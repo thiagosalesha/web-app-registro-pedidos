6b4f73a3b9c9b99085551ee654c1d0b71861ba79
thiagosalesha <thiagosalesha2@gmail.com>
2022-08-26 16:48:40 -0300

    percorrendo Lista

--- mudi/src/main/java/br/com/salesha/mvc/mudi/controller/HomeController.java
+++ mudi/src/main/java/br/com/salesha/mvc/mudi/controller/HomeController.java
@@ -1,13 +1,29 @@
 package br.com.salesha.mvc.mudi.controller;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import br.com.salesha.mvc.mudi.model.Pedido;
 
 @Controller
 public class HomeController {
 	
 	@GetMapping("/home")
-	public String home() {
+	public String home(Model model) {
+		Pedido pedido = new Pedido();
+		pedido.setNomeProd("Smartphone Asus Zenfone 3");
+		pedido.setDescProd("Dual Chip Android 6.0 Tela 5.2\" Snapdragon 16GB 4G Câmera 16MP - Preto Safira");
+		pedido.setUrlProd("https://www.americanas.com.br/produto/131017515");
+		pedido.setUrlImg("https://images-americanas.b2w.io/produtos/01/00/item/131017/5/131017515SZ.jpg");
+		pedido.setValor(new BigDecimal(1190));
+		
+		List<Pedido> lista = Arrays.asList(pedido);
+		model.addAttribute("lista", lista);
 		return "home";
 	}
 }
