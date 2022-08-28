4bb429a63179b7d848780eee2c50ec45aff17d19
thiagosalesha <thiagosalesha2@gmail.com>
2022-08-28 00:05:35 -0300

    modificação na view adicionando style no status

--- mudi/src/main/java/br/com/salesha/mvc/mudi/controller/HomeController.java
+++ mudi/src/main/java/br/com/salesha/mvc/mudi/controller/HomeController.java
@@ -8,20 +8,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.salesha.mvc.mudi.model.Pedido;
+import br.com.salesha.mvc.mudi.model.StatusPedido;
 import br.com.salesha.mvc.mudi.repository.PedidoRepository;
 
 @Controller
+@RequestMapping("/home")
 public class HomeController {
 	
 	@Autowired
 	PedidoRepository pedidoRepository;
 	
-	@GetMapping("/home")
+	@GetMapping
 	public String home(Model model) {
 
 		List<Pedido> lista = pedidoRepository.findAll();
+		model.addAttribute("lista", lista);
+		return "home";
+	}
+	
+	@GetMapping("/{status}")
+	public String aguardando(@PathVariable("status") String status, Model model) {
+
+		List<Pedido> lista = pedidoRepository.findByStatusPedido(StatusPedido.valueOf(status.toUpperCase()));
 		model.addAttribute("lista", lista);
 		return "home";
 	}
--- mudi/src/main/java/br/com/salesha/mvc/mudi/repository/PedidoRepository.java
+++ mudi/src/main/java/br/com/salesha/mvc/mudi/repository/PedidoRepository.java
@@ -1,11 +1,17 @@
 package br.com.salesha.mvc.mudi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.salesha.mvc.mudi.model.Pedido;
+import br.com.salesha.mvc.mudi.model.StatusPedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+
+	List<Pedido> findByStatusPedido(StatusPedido aguardando);
+
 
 }
