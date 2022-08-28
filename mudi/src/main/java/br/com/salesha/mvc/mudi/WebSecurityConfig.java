284495988b0a98b10ce7ba2cbf9026ff0e61c581
thiagosalesha <thiagosalesha2@gmail.com>
2022-08-28 13:55:46 -0300

    implementando Spring Security

---
+++ mudi/src/main/java/br/com/salesha/mvc/mudi/WebSecurityConfig.java
@@ -0,0 +1,41 @@
+package br.com.salesha.mvc.mudi;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+			.anyRequest().authenticated()
+			.and()
+			.httpBasic();
+		
+		
+		return null;
+	}
+	
+	@Bean
+	public UserDetailsService userDetailsService() {
+		UserDetails user =
+			 User.withDefaultPasswordEncoder()
+				.username("henrique")
+				.password("sales")
+				.roles("ADM")
+				.build();
+
+		return new InMemoryUserDetailsManager(user);
+	}
+	
+ }
