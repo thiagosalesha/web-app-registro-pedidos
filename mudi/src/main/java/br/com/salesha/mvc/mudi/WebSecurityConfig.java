d889281040752105caa3b6a30d59440643c2e8a1
thiagosalesha <thiagosalesha2@gmail.com>
2022-08-28 21:17:17 -0300

    implementando login e logout thymeleaf extras

--- mudi/src/main/java/br/com/salesha/mvc/mudi/WebSecurityConfig.java
+++ mudi/src/main/java/br/com/salesha/mvc/mudi/WebSecurityConfig.java
@@ -24,7 +24,8 @@ public class WebSecurityConfig {
 			.formLogin(form -> form
 					.loginPage("/login")
 					.permitAll()
-					);
+					)
+			.logout(logout -> logout.logoutUrl("/logout"));
 		
 		
 		return http.build();
