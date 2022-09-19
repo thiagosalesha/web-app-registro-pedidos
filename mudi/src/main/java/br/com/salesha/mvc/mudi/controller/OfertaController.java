c0a9361f513edbdf94006057a5400578c0ceec70
thiagosalesha <thiagosalesha2@gmail.com>
2022-09-19 13:43:15 -0300

    aplicando vue.js e consumindo api com ajax

--- mudi/src/main/java/br/com/salesha/mvc/mudi/controller/OfertaController.java
+++ mudi/src/main/java/br/com/salesha/mvc/mudi/controller/OfertaController.java
@@ -1,15 +1,29 @@
 package br.com.salesha.mvc.mudi.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.salesha.mvc.mudi.model.Pedido;
+import br.com.salesha.mvc.mudi.model.StatusPedido;
+import br.com.salesha.mvc.mudi.repository.PedidoRepository;
 
 @Controller
 @RequestMapping("/oferta")
 public class OfertaController {
+	
+	@Autowired
+	PedidoRepository pedidoRepository;
 
 	@GetMapping
-	public String getFormularioOferta () {
+	public String getFormularioOferta (Model model) {
+		
+	/*	List<Pedido> lista = pedidoRepository.findAll();
+		model.addAttribute("pedidos", lista); */
 		return "oferta/home";
 	}
 }
